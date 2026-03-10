ca88b3147ab94bbd63f18cce61acd853f60354ed
pomiano <miloszpomian@gmail.com>
2026-03-10 16:38:45 +0100

    comments and tidying up the code

--- src/config.py
+++ src/config.py
@@ -1,63 +1,55 @@
 import taichi as ti
 import math
 
+C = 343.0   # speed of sound in air [m/s]
+FREQ_MAX = 1000.0 # maximum frequency of the wave [Hz]
+SIZE_M = 20.0  # domain size [m]
+DIM = 2  # dimensions (2D)
+
+AMPLITUDE = 10.0 # peak amplitude of the acoustic pressure wave source
+
+# Gaussian Impulse Parameter
+SIGMA = math.sqrt(2 * math.log(2)) / (2 * math.pi * FREQ_MAX) # standard deviation (SIGMA) calculated to fit the maximum frequency
+DELAY_GAUSS = 4 * SIGMA  # delay to ensure the pulse starts smoothly from near-zero amplitude
+
+NODES_PER_WAVELENGTH = 10    # number of grid points per wavelength
+WAVELENGTH = C / FREQ_MAX    # wavelength [m]
+DX = WAVELENGTH / NODES_PER_WAVELENGTH   # spatial step [m]
+N = int(SIZE_M / DX)     # interior grid points in one dimension
+N_2 = N + 2 # grid size including a 1-node boundary layer on all sides for calculations
+
+# source position
+SRC_X = N_2 // 2
+SRC_Y = N_2 // 2
+
+SAFETY_FACTOR = 0.99 # stability safety factor
+COURANT = (1.0 / math.sqrt(DIM)) * SAFETY_FACTOR # Courant number
+COURANT_SQ = COURANT**2 # squared Courant number
+
+# calculates the maximum stable time step (DT) to satisfy the CFL condition and prevent simulation instability
 def get_time_step(dimensions, dx, speed, safety_factor):
     courant_limit = 1.0 / math.sqrt(dimensions)
     return (dx / speed) * courant_limit * safety_factor
 
-# Parametry srodowiska
-C = 343.0   # predkosc [m/s]
-SIZE_M = 20.0  # rozmiar [m]
-DIM = 2  # 2D
+DT = get_time_step(DIM, DX, C, SAFETY_FACTOR) # time step [s]
 
-# Parametry zrodla
-FREQ_MAX = 1000.0 # maksymalna czestotliwpsc fali [Hz]
-AMPLITUDE = 10.0
-
-#Parametry czasowe impulsu
-SIGMA = math.sqrt(2 * math.log(2)) / (2 * math.pi * FREQ_MAX) # sigma dla impulsu Gaussa
-DELAY_GAUSS = 4 * SIGMA # opóźnienie startu sygnału
-
-# Dyskretyzacja przestrzenna
-NODES_PER_WAVELENGTH = 10    # ilsoc punktow na dlugosc fali
-WAVELENGTH = C / FREQ_MAX    # dlugosc fali [m]
-DX = WAVELENGTH / NODES_PER_WAVELENGTH   # krok odleglosciowy [m]
-
-# Obliczanie liczby wezlow
-N = int(SIZE_M / DX)     # ilosc punktow w jednym wymiarze
-N_2 = N + 2 # dodajemy po jednym punckie na kazda strone do obliczen
-
-# Polozenie zrodla
-SRC_X = N_2 // 2
-SRC_Y = N_2 // 2
-
-# Wspolczynnik bezpieczenstwa
-SAFETY_FACTOR = 0.99
-
-# Warunek Couranta
-COURANT = (1.0 / math.sqrt(DIM)) * SAFETY_FACTOR
-COURANT_SQ = COURANT**2
-
-# Obliczanie kroku czasowego
-DT = get_time_step(DIM, DX, C, SAFETY_FACTOR)
-
-# wspolczynniki alfa dla materiałów przy fali o czestotliowsci 500 HZ
+# material absorption coefficients at 500 Hz
 # https://www.acoustic-supplies.com/absorption-coefficient-chart/
 MATERIALS = {
     "brick": 0.03, # brick(natural)
     "wooden_bench": 0.76 # Benches (wooden, fully occupied)
 }
 
-# Parametry animacji
-frame_duration = 0.02 # czas trwania jednej klatki wizualizacji [s]
+frame_duration = 0.02 # fixed time interval for GUI visualization frames [s]
 
 
 def print_config():
     print("-" * 30)
-    print(f"SYMULACJA FDTD {DIM}D")
+    print(f"FDTD SIMULATION CONFIGURATION {DIM}D")
     print("-" * 30)
-    print(f"Rozmiar siatki:  {N} x {N}")
-    print(f"Krok DX:         {DX*1000:.2f} mm")
-    print(f"Krok DT:         {DT*1e6:.2f} us")
-    print(f"Liczba Couranta: {COURANT:.4f} (Max: {1/math.sqrt(DIM):.4f})")
+    print(f"Grid Size:  {N} x {N} nodes")
+    print(f"Spatial Step (DX):         {DX*1000:.2f} mm")
+    print(f"Time Step (DT):         {DT*1e6:.2f} us")
+    print(f"Courant Number: {COURANT:.4f} (Limit: {1/math.sqrt(DIM):.4f})")
+    print(f"Max Frequency:     {FREQ_MAX} Hz")
     print("-" * 30)
--- src/main.py
+++ src/main.py
@@ -1,50 +1,65 @@
 import taichi as ti
-from config import N,COURANT_SQ, N_2, MATERIALS,COURANT, SIGMA, AMPLITUDE, SRC_X, SRC_Y, DT, frame_duration, DELAY_GAUSS, print_config
+from config import COURANT_SQ, N_2, MATERIALS,COURANT, SIGMA, AMPLITUDE, SRC_X, SRC_Y, DT, frame_duration, DELAY_GAUSS, print_config
 import numpy as np
 import time
 
 ti.init(arch=ti.gpu)
 
-p_old = ti.field(ti.f32, shape=(N_2, N_2)) # macierz ciśnień, do niej wsadzane są najnowsze wartosci
+# pressure fields for time stepping (p_old and p_curr)
+p_old = ti.field(ti.f32, shape=(N_2, N_2))
 p_curr = ti.field(ti.f32, shape=(N_2, N_2))
 
-k_field = ti.field(dtype=ti.i32, shape=(N_2, N_2)) #ilosc sasiadow
-alpha_field = ti.field(dtype=ti.f32, shape=(N_2, N_2)) # współczynniki pochlaniania
-bk_field = ti.field(dtype=ti.f32, shape=(N_2, N_2)) # macierz bk
+# matrix representing the count of neighboring nodes for each grid point
+k_field = ti.field(dtype=ti.i32, shape=(N_2, N_2))
+
+# matrix field for local absorption coefficients used to implement material losses
+alpha_field = ti.field(dtype=ti.f32, shape=(N_2, N_2))
+
+# matrix of BK values used for simplified boundary condition calculations
+bk_field = ti.field(dtype=ti.f32, shape=(N_2, N_2))
+
 
 @ti.kernel
 def generation_symulation_map():
-    for i,j in k_field: # domyslnie wszedzie 4 sąsiadów i otwarta przestrzen
-        k_field[i , j] = 4
-        alpha_field [i, j] = 1.0
+    for i,j in k_field:
+        k_field[i , j] = 4 # default 4 neighbours
+        alpha_field [i, j] = 0.0 # air alpha =0
 
-    for i,j in k_field: # warstwa zewnetrzna potrzebna do obliczeń
+    # set boundary nodes to 0 neighbors to define the computational domain limits
+    for i,j in k_field:
         if i == 0 or j == 0 or i == N_2 - 1 or j == N_2 - 1:
             k_field[i, j] = 0
             alpha_field[i, j] = 0.0
 
-    for i,j in k_field: # sciany
+    # walls
+    for i,j in k_field:
+        # corner nodes: only 2 adjacent neighbors within the domain.
         if((i==1 and j ==1) or (i==1 and j==N_2-2) or (i==N_2-2 and j==1) or (i==N_2-2 and j==N_2-2)):
             k_field[i, j] = 2
             alpha_field[i, j] = MATERIALS["brick"]
 
+        # vertical walls: 3 neighbors
         elif (i == 1 or i == N_2 - 2) and (j > 0 and j < N_2 -1):
             k_field[i, j] = 3
             alpha_field[i, j] = MATERIALS["brick"]
 
+        # horizontal walls: 3 neighbors
         elif (j == 1 or j == N_2 - 2) and (i > 0 and i < N_2 -1):
             k_field[i, j] = 3
             alpha_field[i, j] = MATERIALS["brick"]
 
-    # wymiary przeszkód
+    # Internal Obstacle
+    # coordinates defining the boundaries of the obstacle
     r_x1, r_x2 = 100, 200
     r_y1, r_y2 = 100, 200
 
-    for i,j in k_field:  # przeszkoda
+    for i,j in k_field:
+        # interior of the obstacle: No wave propagation
         if i > r_x1 and i < r_x2 and j > r_y1 and j < r_y2:
             k_field[i, j] = 0
             alpha_field[i, j] = 0.0
 
+        # obstacle corners: 4 neighbors
         elif (i == r_x1 and j == r_y1) or \
                 (i == r_x1 and j == r_y2) or \
                 (i == r_x2 and j == r_y1) or \
@@ -52,26 +67,30 @@ def generation_symulation_map():
             k_field[i, j] = 4
             alpha_field[i, j] = MATERIALS["wooden_bench"]
 
+        # obstacle vertical edges: 3 neighbors
         elif (i == r_x1 or i == r_x2) and (j > r_y1 and j < r_y2):
             k_field[i, j] = 3
             alpha_field[i, j] = MATERIALS["wooden_bench"]
 
+        # obstacle horizontal edges: 3 neighbors
         elif (j == r_y1 or j == r_y2) and (i > r_x1 and i < r_x2):
             k_field[i, j] = 3
             alpha_field[i, j] = MATERIALS["wooden_bench"]
 
+    # precalculate the Bk matrix used for boundary conditions and material absorption
     for i,j in bk_field:
         k_val = k_field[i, j]
         beta_val = beta_from_alpha(alpha_field[i, j])
-
         bk_field[i, j] = (4.0 - float(k_val)) * COURANT * beta_val / 2.0
+
 
 @ti.func
 def beta_from_alpha(alpha):
+    # converts the absorption coefficient (alpha) to the reflection-based beta factor
     Beta = 0.0
-    if alpha >= 1.0:  # otwarta przestrzeń
+    if alpha >= 1.0:  # open space (total absorption)
         Beta = 1.0
-    elif alpha <= 0.0:    # idealna ściana
+    elif alpha <= 0.0:    # ideal wall (total reflection)
         Beta = 0.0
     else:
         R = ti.sqrt(1.0 - alpha)
@@ -85,63 +104,86 @@ def step(p_prev: ti.template(), p_now: ti.template(), steps: int):
         if k_field[i , j] > 0:
             k_val = k_field[i,j]
             bk_val = bk_field[i,j]
-            w1 = (2.0 - float(k_val) * COURANT_SQ) * p_now[i, j]
-            w2 = (bk_val - 1.0) * p_prev[i, j]
-            w3 = COURANT_SQ * (p_now[i + 1, j] + p_now[i - 1, j] + p_now[i, j + 1] + p_now[i, j - 1])
-            p_prev[i, j] = (w1 + w2 + w3) / (1 + bk_val)
+
+            # discretized acoustic wave equation optimized for GPU computation
+            current_term = (2.0 - float(k_val) * COURANT_SQ) * p_now[i, j]
+            loss_term = (bk_val - 1.0) * p_prev[i, j]
+            laplacian_term = COURANT_SQ * (p_now[i + 1, j] + p_now[i - 1, j] + p_now[i, j + 1] + p_now[i, j - 1])
+
+            # update pressure for the next time step
+            p_prev[i, j] = (current_term + loss_term + laplacian_term) / (1 + bk_val)
         else:
+            # nodes with 0 neighbors remain at zero
             p_prev[i, j] = 0
 
-    #ZRODLO
+    # Acoustic Source
+    # gaussian pulse calculation for a smooth, band-limited signal
     pulse = AMPLITUDE * ti.exp(- ((DT*steps - DT*DELAY_GAUSS)**2) / (2 * SIGMA**2))
     p_prev[SRC_X, SRC_Y] += pulse # soft source
 
 
 def main():
-    gui = ti.GUI("2d - fdtd", res=(N_2, N_2))
+    gui = ti.GUI("2D FDTD Simulation - Neumann", res=(N_2, N_2))
     print_config()
-    generation_symulation_map()
+    generation_symulation_map() # initialize grid geometry and material fields
+
+    # set up a buffer swap mechanism to alternate between past and current pressure fields
     buffers = [p_old, p_curr]
 
     prev_time = time.time()
-
     steps = 0
     accumulator = 0
 
+    # create a boolean mask for visualization to highlight wall/obstacle locations
     alpha_map = alpha_field.to_numpy()
-    is_wall = alpha_map < 1.0
+    is_wall = alpha_map > 0.01
 
     is_paused = False
     while gui.running:
+        # event handling for pausing the simulation with the SPACE key
         if gui.get_event(ti.GUI.PRESS):
             if gui.event.key == ti.GUI.SPACE:
                 is_paused = not is_paused
 
+        # calculate elapsed real time to maintain a constant frame rate
         now = time.time()
         delta_real_time = now - prev_time
         prev_time = now
 
         accumulator+=delta_real_time
+
+        # only update the simulation and GUI if the frame duration interval has passed
         if(accumulator >= frame_duration):
             if not is_paused:
                 steps += 1
+
+                # To save memory, we use only two matrices instead of three by swapping their roles
+                # This approach reuses the memory of the "old" buffer to store the "future" step
                 b_old = steps % 2
                 b_curr = (steps + 1) % 2
+
+                # compute next time step
                 step(buffers[b_old], buffers[b_curr], steps)
 
             p_np = buffers[b_old].to_numpy()
             max_val = np.abs(p_np).max()
-            p_normalized = p_np / (max_val + 1e-8) # normalizujemy poniewaz chcemy uzyskac wartosci od -1 do 1
-            gray_val = p_normalized * 0.5 + 0.5 # uzyskujemy wartoci od 0 do 1
 
-            gray_val = np.clip(gray_val, 0.0, 1.0) # zabezpieczenie choc i tak juz wczesniej znormalizowane
+            # Normalize pressure values to the range [-1, 1]
+            p_normalized = p_np / (max_val + 1e-8)
+            # normalize pressure to [0, 1] range for grayscale display
+            gray_val = p_normalized * 0.5 + 0.5
+
+            gray_val = np.clip(gray_val, 0.0, 1.0)
             img = np.dstack((gray_val, gray_val, gray_val))
-            img[is_wall, 1] *= 0.3 # zielony na 30%
-            img[is_wall, 2] *= 0.3 # niebieski na 30%
+
+            # colorize walls
+            img[is_wall, 1] *= 0.3 # green 30%
+            img[is_wall, 2] *= 0.3 # blue 30%
 
             gui.set_image(img)
             gui.show()
 
+            # reset the timer accumulator for the next frame
             accumulator = 0
 
 
